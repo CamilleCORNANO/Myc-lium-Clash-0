f5ad2b92efeb77d838c202a96dfde94efb10e121
Camille <macacornano@gmail.com>
2025-12-02 16:57:16 +0100

    lil modifs

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-   
+
 from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017")
-db = client["todo_db"]
+db = client["Mycelium_Clash"]
 characters = db["characters"]
 monsters = db["monsters"]
 
